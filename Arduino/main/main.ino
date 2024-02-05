eda6c52cd26a70b126883169bde082c77a20e18c
Alex Nordstrom <alexanderjnordstrom@gmail.com>
2024-02-04 22:20:29 -0800

    Refactored arduino code and added initial mute button functionality

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -17,49 +17,146 @@
    4. "value3"
         Type: int
 */
-
+// =============================
+// Libraries
+// =============================
 #include <HID-Project.h>
 #include <HashMap.h>
+#include <Adafruit_NeoPixel.h>
 
-#define MAP_SIZE 10
+// =============================
+// Pre-Processor Variables
+// =============================
+// Map Variables
+#define MAP_SIZE 16
 
-using ActuatorMapType = HashMap<int, int>;
+// LED Variables
+#define LED_PIN 7
+#define LED_COUNT 2
+#define BRIGHTNESS 200
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
 
 void sendActuatorEvent(long actuatorId, long actuatorValue, long value3);
 void evaluateAndSendActuatorValue(int actuatorId, int currentValue);
 
-ActuatorMapType actuatorMap = ActuatorMapType(MAP_SIZE);
+HashMap<int, int> actuatorPinMap = HashMap<int, int>(MAP_SIZE);
+HashMap<int, int> actuatorValueMap = HashMap<int, int>(MAP_SIZE);
 volatile boolean newSignal = false;
 int potTolerance = 6;
 
-void setup() {
-  actuatorMap[0](1, 0);
-  actuatorMap[1](2, 0);
-  actuatorMap[2](3, 0);
-  actuatorMap[3](4, 0);
+uint8_t incomingHIDBuffer[255];
 
+int pots[5] = {100, 101, 102, 103, 104};
+int momSwitches[5] = {200, 201, 202, 203, 204};
+
+void setup() {
+  RawHID.begin(incomingHIDBuffer, sizeof(incomingHIDBuffer));
+  
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(BRIGHTNESS);
+
+  strip.setPixelColor(0, strip.Color(0, 150, 0)); 
+  strip.setPixelColor(1, strip.Color(150, 0, 0));
+  strip.show();
+
+  // For LED, // Pin 0 is main LED, the use addresses 1, 3, 5 for neopixel chaining
+  // For LED, use neopixel array address
+
+  // =======================
+  // Actuator Groups
+  // =======================
+  actuatorPinMap[0](100, A0);
+  actuatorPinMap[1](200, 3); // Digital Push Button pins unmapped
+  actuatorPinMap[2](300, 1);
+
+  actuatorPinMap[3](101, A1);
+  actuatorPinMap[4](201, 4);
+  actuatorPinMap[5](301, 2);
+
+  actuatorPinMap[6](102, A2);
+  actuatorPinMap[7](202, -1);
+  actuatorPinMap[8](302, 3);
+
+  actuatorPinMap[9](103, A3);
+  actuatorPinMap[10](203, -1);
+  actuatorPinMap[11](303, 4);
+
+  actuatorPinMap[12](104, A9);
+  actuatorPinMap[13](204, -1);
+  actuatorPinMap[14](304, 5);
+
+  actuatorValueMap[0](100, 0);
+  actuatorValueMap[1](101, 0);
+  actuatorValueMap[2](102, 0);
+  actuatorValueMap[3](103, 0);
+  actuatorValueMap[4](104, 0);
+
+  actuatorValueMap[5](200, 1);
+  actuatorValueMap[6](201, 1);
+  actuatorValueMap[7](202, 1);
+  actuatorValueMap[8](203, 1);
+  actuatorValueMap[9](204, 1);
+  
   pinMode(3, INPUT_PULLUP);
   pinMode(4, INPUT_PULLUP);
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println("Pin 3: ");
-  Serial.println(digitalRead(3));
-  Serial.println("Pin 4: ");
-  Serial.println(digitalRead(4));
-  Serial.println("");
-  
+  for (int p = 0; p < 5; p++) {
+    evalActuator("pot", pots[p]);
+  }
+
+  for (int m = 0; m < 5; m++) {
+    evalActuator("mom_switch", momSwitches[m]);
+  }
+
+  /*
   int pot1CurrentValue = analogRead(A0);
   int pot2CurrentValue = analogRead(A1);
   int pot3CurrentValue = analogRead(A2);
   int pot4CurrentValue = analogRead(A3);
+  int pot5CurrentValue = analogRead(A9);
   
-  evaluateAndSendActuatorValue(1, pot1CurrentValue);
-  evaluateAndSendActuatorValue(2, pot2CurrentValue);
-  evaluateAndSendActuatorValue(3, pot3CurrentValue);
-  evaluateAndSendActuatorValue(4, pot4CurrentValue);
-  delay(100);
+  evaluateAndSendActuatorValue(100, pot1CurrentValue);
+  evaluateAndSendActuatorValue(200, pot2CurrentValue);
+  evaluateAndSendActuatorValue(300, pot3CurrentValue);
+  evaluateAndSendActuatorValue(400, pot4CurrentValue);
+  evaluateAndSendActuatorValue(500, pot5CurrentValue);
+  */
+
+  // parseHidEventV1();
+
+  delay(50);
+}
+
+void parseHidEventV1 () {
+  int bytesAvailable = RawHID.available();
+  if (bytesAvailable > 0) {
+      String message = ""; // Create a string to hold your message
+      // Use a for loop to ensure you read all available bytes
+      for (int i = 0; i < bytesAvailable; i++) {
+          char c = (char)RawHID.read(); // Read a byte and cast it to a character
+          message += c; // Append the character to your message string
+      }
+      Serial.println(message); // Print the complete message
+  }
+}
+
+void parseHidEventV2 () {
+  if (RawHID.available() > 0) {
+        String message = "";
+        // Continuously read data until there's none left
+        while (RawHID.available() > 0) {
+            char c = (char)RawHID.read(); // Read a byte and cast it to a character
+            message += c; // Append the character to your message string
+        }
+        if (message.length() > 0) {
+            Serial.println(message); // Print the complete message
+        }
+    }
 }
 
 void sendActuatorEvent(long actuatorId, long actuatorValue, long value3) {
@@ -74,15 +171,40 @@ void sendActuatorEvent(long actuatorId, long actuatorValue, long value3) {
   RawHID.write(data, sizeof(data));
 }
 
-void evaluateAndSendActuatorValue(int actuatorId, int currentValue) {
-    int actuatorIndex = actuatorMap.getIndexOf(actuatorId);
+void evalActuator(String type, int actuatorId) {
+    // Get Actuator Pin
+    int actPinIdx = actuatorPinMap.getIndexOf(actuatorId);
+    if (actPinIdx == -1) return;
+    int actPin = actuatorPinMap.getValueOf(actuatorId);
+    if (actPin == -1) return;
+
+    // Get Actuator Stored Value
+    int actuatorIndex = actuatorValueMap.getIndexOf(actuatorId);
     if (actuatorIndex == -1) return;
-    int storedValue = actuatorMap.getValueOf(actuatorId);
-    // if (abs(storedValue - currentValue) > potTolerance) {
-    if (currentValue <= storedValue - potTolerance || currentValue >= storedValue + potTolerance) {
-        // String str1 = String("Actuator: ") + actuatorId + String("  New value: ") + currentValue + String("   Old value: ") + storedValue;
-        // Serial.println(str1);
-        actuatorMap[actuatorIndex].setValue(currentValue);
-        sendActuatorEvent(actuatorId, currentValue, 0);
+    int storedValue = actuatorValueMap.getValueOf(actuatorId);
+   
+    if (type == "pot") {
+      int potVal = analogRead(actPin);
+      if (potVal <= storedValue - potTolerance || potVal >= storedValue + potTolerance) {
+          actuatorValueMap[actuatorIndex].setValue(potVal);
+          sendActuatorEvent(actuatorId, potVal, 0);
+      }
     }
+    if (type == "mom_switch") {
+      // If last value is 0 and current value is 0, early exit
+      // If last value is 1 and current value is 0, set last value + send message
+      // If last value is 0 and current value is 1, set last value
+      // If last value is 1 and current value is 1, early exit
+      int msVal = digitalRead(actPin);
+      if (storedValue == 0 && msVal == 0) return;
+
+      if (msVal == 0 && ) {
+        actuatorValueMap[actuatorIndex].setValue(potVal);
+        sendActuatorEvent(actuatorId, msVal, 0);
+      }
+    }
+
 }
+
+// String str1 = String("Actuator: ") + actuatorId + String("  New value: ") + potVal + String("   Old value: ") + storedValue;
+// Serial.println(str1);
